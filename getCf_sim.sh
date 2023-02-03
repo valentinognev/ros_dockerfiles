395c0bbb0be31d3371ac8ef18e16c2c0b957da96
valento81 <valento@rafael.co.il>
2023-02-03 11:53:04 +0200

    sim_cf script

---
+++ getCf_sim.sh
@@ -0,0 +1,50 @@
+
+git clone https://github.com/ethz-asl/mav_comm
+git clone git@github.com:rnd-hub/cf_sim.git --recursive
+
+cd cf_sim/crazyflie_ros/
+git clone git@github.com:rnd-hub/crazyflie_cpp.git 
+cd crazyflie_cpp
+git checkout 25bc72c120f8cea6664dd24e334eefeb7c9606ca
+cd ../../cf_app_layer
+git clone git@github.com:rnd-hub/cf_firmware_new.git
+cd cf_firmware_new
+git checkout f8a62425b7ac2b7bc120f77d70ac3037ebe73605
+cd ..
+
+
+sed -i 's%CV_LOAD_IMAGE_GRAYSCALE%cv::IMREAD_GRAYSCALE%' /home/valentin/catkin_ws/src/cf_sim/sim_cf_gazebo/src/gazebo_odometry_plugin.cpp
+sed -i 's% -Werror -Werror % -Werror -Wno-error=address-of-packed-member %' /home/valentin/catkin_ws/src/cf_sim/sim_cf_firmware/sitl_make/CMakeLists.txt
+
+cp /home/valento/catkin_ws/src/cf_sim/sim_cf_firmware/version.c /home/valento/catkin_ws/src/cf_sim/cf_app_layer/
+cd /home/valento/catkin_ws/src/cf_sim/cf_app_layer/sitl_make
+mkdir build 
+cd build
+cmake ..
+make
+
+
+
+
+
+# 
+# 
+# 
+# cd ..
+# git clone git@github.com:rnd-hub/crazyflie_tools.git --recursive
+# cd crazyflie_tools
+# git checkout e7a8893ad63a04b63891e73244a3623131bbdc68
+# 
+# cd ../../sim_cf_firmware
+# git clone git@github.com:rnd-hub/cf_app_layer.git --recursive
+# cd cf_app_layer && git checkout 1d4e46ae259f8b4c185309d3e0f3463fdbc7fb22
+# 
+# 
+# 
+# 
+# git clone git@github.com:rnd-hub/cf_sim.git --recursive
+# 
+# cd cf_sim//crazyflie_ros/
+# 
+# 
+# 
